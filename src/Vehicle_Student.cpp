3ccc56b9ce1d14f0e4ef796a32a6624818ec3cb9
root <xiaomajsj@gmail.com>
2020-06-11 15:21:41 +0000

    L1 finished

--- src/Vehicle_Student.cpp
+++ src/Vehicle_Student.cpp
@@ -25,6 +25,9 @@ void Vehicle::setCurrentDestination(std::shared_ptr<Intersection> destination)
 void Vehicle::simulate()
 {
     // Task L1.2 : Start a thread with the member function „drive“ and the object „this“ as the launch parameters. 
+    std::thread t=std::thread(&Vehicle::drive,this);
+    _threads.emplace_back(std::move(t));
+
     // Also, add the created thread into the _thread vector of the parent class. 
 }
 
